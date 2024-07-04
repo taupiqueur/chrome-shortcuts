f1c9fa262204beeca087dff330f932bed073da8f
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2024-07-04 13:03:33 +0200

    Add a “copy to clipboard” button to code blocks

---
+++ src/manual/manual.js
@@ -0,0 +1,26 @@
+const buttonElements = document.querySelectorAll('button')
+
+for (const buttonElement of buttonElements) {
+  const actionName = buttonElement.dataset.action
+
+  switch (actionName) {
+    case 'copyToClipboard':
+      buttonElement.addEventListener('click', () => {
+        navigator.clipboard
+          .writeText(buttonElement.value)
+          .then(() => {
+            buttonElement.textContent = buttonElement.dataset.copyFeedback
+            setTimeout(() => {
+              buttonElement.textContent = buttonElement.ariaLabel
+            }, 1000)
+          })
+      })
+      break
+
+    default:
+      console.error(
+        'Unknown action: "%s"',
+        actionName
+      )
+  }
+}
